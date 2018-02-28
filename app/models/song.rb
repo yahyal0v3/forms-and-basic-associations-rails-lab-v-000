bc1f4904b5a921abd3ba0c060badb1110c05ca3f
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-28 21:00:57 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -19,7 +19,9 @@ class Song < ActiveRecord::Base
     self.genre.name if self.artist
   end
 
-  def note_contents
-
+  def note_contents=(content)
+    content.each do |c|
+      note = Note.new(content: c)
+      self.notes.content << note
   end
 end
